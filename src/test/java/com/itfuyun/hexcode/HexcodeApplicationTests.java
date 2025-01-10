12fced1618f104b3ac45fc6020acbafa1ab5d149
tanxh <itfuyun@gmail.com>
2025-01-10 14:36:25 +0800

    init

---
+++ src/test/java/com/itfuyun/hexcode/HexcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itfuyun.hexcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
